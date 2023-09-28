3e801c1d78c1530097cc9c3258b2549edb9aa649
Nok Chan <nok.lam.chan@quantumblack.com>
2023-09-28 02:49:39 +0100

    update
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- kedro_viz_lite/_modidx.py
+++ kedro_viz_lite/_modidx.py
@@ -5,5 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://noklam.github.io',
                 'git_url': 'https://github.com/noklam/kedro-viz-lite',
                 'lib_path': 'kedro_viz_lite'},
-  'syms': { 'kedro_viz_lite.core': {'kedro_viz_lite.core.foo': ('core.html#foo', 'kedro_viz_lite/core.py')},
-            'kedro_viz_lite.hook': {'kedro_viz_lite.hook.foo': ('hook.html#foo', 'kedro_viz_lite/hook.py')}}}
+  'syms': { 'kedro_viz_lite.core': { 'kedro_viz_lite.core.prepare_dag_json': ('core.html#prepare_dag_json', 'kedro_viz_lite/core.py'),
+                                     'kedro_viz_lite.core.run_viz_lite': ('core.html#run_viz_lite', 'kedro_viz_lite/core.py')}}}
